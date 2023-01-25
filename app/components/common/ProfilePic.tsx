6db27a9f16977c5365846d6142425259a21237f2
William Gonçalves <euwilliamgoncalves@gmail.com>
2023-01-25 20:12:37 -0300

    refactor: add className prop on ProfilePic

--- app/components/common/ProfilePic.tsx
+++ app/components/common/ProfilePic.tsx
@@ -1,6 +1,16 @@
-export function ProfilePic({ src, alt }: { src: string; alt: string }) {
+export function ProfilePic({
+  src,
+  alt,
+  className,
+}: {
+  src: string;
+  alt: string;
+  className?: string;
+}) {
   return (
-    <div className="animate-from-barely-visible non-animate relative mx-auto h-[50vw] w-[50vw] animation-duration-1000 lg:h-[40vw] lg:w-[40vw] xl:h-[30vw] xl:w-[30vw]">
+    <div
+      className={`animate-from-barely-visible non-animate relative mx-auto h-[50vw] w-[50vw] animation-duration-1000 lg:h-[40vw] lg:w-[40vw] xl:h-[30vw] xl:w-[30vw] ${className}`}
+    >
       <div className="absolute h-full w-full animate-pulse rounded-full border-[1rem] border-primary-focus"></div>
       <img
         className="h-full w-full rounded-full border-[1rem] border-base-100 object-cover"
