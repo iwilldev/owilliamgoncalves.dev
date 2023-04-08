bb2adeadd2058bc3ca619fee255380c8047ebecf
William Gonçalves <euwilliamgoncalves@gmail.com>
2023-04-07 23:05:23 -0300

    feat: scroll to top behavior on Main

--- app/components/layout/Main.tsx
+++ app/components/layout/Main.tsx
@@ -1,3 +1,6 @@
+import { useLocation } from "@remix-run/react";
+import { useEffect, useRef } from "react";
+
 export function Main({
   menuOpened,
   children,
@@ -5,11 +8,21 @@ export function Main({
   menuOpened: boolean;
   children: React.ReactNode;
 }) {
+  const { pathname } = useLocation();
+  const mainRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    mainRef.current?.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto", // Optional if you want to skip the scrolling animation
+    });
+  }, [pathname]);
   const openMenuClasses = menuOpened
     ? "fixed origin-center -translate-x-[50vw] lg:-translate-x-[20vw] scale-75 pointer-events-none saturate-0"
     : "";
   return (
     <main
+      ref={mainRef}
       className={`absolute h-screen w-screen overflow-x-hidden bg-base-100 transition-all duration-700 scrollbar-thin scrollbar-track-base-300 scrollbar-thumb-primary ${
         openMenuClasses
       }`}
